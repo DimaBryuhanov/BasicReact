c4278a9873a8a12957f608aa3b3126d6b40bd4dc
User <user@Users-MacBook-Pro.local>
2020-09-10 19:32:16 +0300

    added: props

--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@
 let name = "Mark";
 
 const styles = {
+    root: {
+        margin: "15px",
+        fontFamily:"Times"
+    },
     title: {
         color: "red"
     }
 }
 
 const WelcomeComponent = (props) => {
+    console.log(props);
     return (
-        <h1 className="title" style={styles.title}>Welcome {props.name}!</h1>
+        <div style={styles.root}>
+            <h1 className="title" style={styles.title}>Welcome, {props.name}!</h1>
+            <p>{props.message}</p>
+        </div>
+
     )
 }
 
 ReactDOM.render(
-    
-    <WelcomeComponent 
+
+    <WelcomeComponent
         name={name}
+        message={"Enjoy the course!"}
     />,
     document.getElementById("root")
 );
