30536b8497b62754ff9b4abcabc58329958c7a6c
User <user@Users-MacBook-Pro.local>
2020-09-10 18:46:45 +0300

    added: style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 
 ReactDOM.render(
-    React.createElement("h1",null, "Hello World"),
+    React.createElement(
+        "h1",
+        {"style":{"color":"red"}},
+        "Hello World"),
     document.getElementById("root")
 );
